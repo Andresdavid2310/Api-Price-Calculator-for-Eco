801c24a79eba57c635859c18ccf3c028a23fe4c9
Andresdavid2310 <dandavi2310@hotmail.com>
2023-10-14 20:13:39 +0200

    init config

---
+++ src/main/java/com/api/pricesCalculator/PricesCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.api.pricesCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/pricesCalculator/PricesCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pricesCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
