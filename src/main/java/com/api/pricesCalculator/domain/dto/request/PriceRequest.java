876ae6a77906874d16adf83f63d6c31509839489
Andresdavid2310 <dandavi2310@hotmail.com>
2023-10-16 15:54:54 +0200

    add documentation to swagger and refactor the entities

--- src/main/java/com/api/pricesCalculator/domain/dto/request/PriceRequest.java
+++ src/main/java/com/api/pricesCalculator/domain/dto/request/PriceRequest.java
@@ -1,5 +1,7 @@
 package com.api.pricesCalculator.domain.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.Min;
@@ -11,15 +13,19 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "PriceRequest", description = "Solicitud de precio")
 public class PriceRequest {
     @NotNull(message = "brandId requerido!")
     @Min(1)
+    @ApiModelProperty(value = "Identificación de la marca", example = "1")
     private Long brandId;
 
     @NotNull(message = "productId requerido!")
     @Min(1)
+    @ApiModelProperty(value = "Identificación del producto", example = "35455")
     private Long productId;
 
     @NotNull(message = "applicationDate requerido!")
+    @ApiModelProperty(value = "Fecha de solicitud", example = "2023-10-15T10:30:00")
     private LocalDateTime applicationDate;
 }
--- src/main/java/com/api/pricesCalculator/domain/model/entity/Price.java
+++ src/main/java/com/api/pricesCalculator/domain/model/entity/Price.java
@@ -1,56 +1,27 @@
-package com.api.pricesCalculator.domain.model.entity;
+    package com.api.pricesCalculator.domain.model.entity;
 
-import lombok.Data;
+    import lombok.Data;
 
-import javax.persistence.*;
+    import javax.persistence.*;
 
 
-@Data
-@Entity
-@Table(name = "PRICE")
-public class Price {
-    @Id
-    @Column(name = "PRICE_ID")
-    private Long id;
+    @Data
+    @Entity
+    @Table(name = "PRICE")
+    public class Price {
+        @Id
+        @Column(name = "PRICE_ID")
+        private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "BRAND_ID", nullable = false)
-    private Brand brand;
+        @ManyToOne
+        @JoinColumn(name = "BRAND_ID", nullable = false)
+        private Brand brand;
 
-    @ManyToOne
-    @JoinColumn(name = "PRODUCT_ID", nullable = false)
-    private Product product;
+        @ManyToOne
+        @JoinColumn(name = "PRODUCT_ID", nullable = false)
+        private Product product;
 
-    @ManyToOne
-    @JoinColumn(name = "PRICE_LIST",nullable = false)
-    private PriceDetail priceDetails;
-
-    /*@Id
-    @Column(name = "PRICE_ID")
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "BRAND_ID", nullable = false)
-    private Brand brand;
-
-    @Column(name = "START_DATE")
-    private LocalDateTime startDate;
-
-    @Column(name = "END_DATE")
-    private LocalDateTime endDate;
-
-    @Column(name = "PRICE_LIST")
-    private Long priceList;
-
-    @Column(name = "PRODUCT_ID")
-    private Long productId;
-
-    @Column(name = "PRIORITY")
-    private Integer priority;
-
-    @Column(name = "PRICE")
-    private Double price;
-
-    @Column(name = "CURR")
-    private String curr;*/
-}
+        @ManyToOne
+        @JoinColumn(name = "PRICE_LIST",nullable = false)
+        private PriceDetail priceDetails;
+    }
